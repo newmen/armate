451174131de6322916bde5bc30c45142d2a910f6
Gleb Averchuk <altermn@gmail.com>
2024-10-03 21:11:10 +0300

    add logging dependencies and jvm opts

--- project.clj
+++ project.clj
@@ -2,5 +2,8 @@
   :description "The linter for PlantUML (ArchiMate) diagrams"
   :license {:name "Private"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/math.combinatorics "0.3.0"]]
+                 [org.clojure/math.combinatorics "0.3.0"]
+                 [org.clojure/tools.logging "1.3.0"]
+                 [ch.qos.logback/logback-classic "1.5.8"]]
+  :jvm-opts ["-Dcasc.yaml.max.aliases=\"100\""]
   :repl-options {:init-ns armate.archimate})
