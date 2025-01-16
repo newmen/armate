84759abe5147aab2929497e77019259dea515d96
Gleb Averchuk <altermn@gmail.com>
2025-01-16 22:20:57 +0300

    add forgotten archimate/viz/common.clj

---
+++ src/armate/archimate/viz/common.clj
@@ -0,0 +1,15 @@
+(ns armate.archimate.viz.common
+   (:require [clojure.string :as s]))
+
+(defn- combine-fn-name
+  [parts]
+  (let [parts2 (map s/capitalize parts)]
+    (if (= 1 (count parts))
+      (str "Other_" (first parts2))
+      (str (first parts2) "_" (s/join (rest parts2))))))
+
+(defn get-fn-name
+  [kind]
+  (-> kind
+      (s/split #"-")
+      (combine-fn-name)))
